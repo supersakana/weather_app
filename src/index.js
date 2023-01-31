612d42b3a83a9fd12dd59969e09db0f932f79e06
supersakana <zacwilliamson@icloud.com>
2023-01-31 14:53:33 -0600

    search input is cleared after submission and results are updated with new search

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './style.css';
 
 document.querySelector('#submit').addEventListener('click', async (e) => {
   e.preventDefault();
+  const results = document.querySelector('#results');
+  results.innerHTML = '';
+
   const city = document.querySelector('#search');
   const data = await callApi(city.value);
-  document.querySelector('#results').append(`${data.name}, ${data.weather[0].main}, Current: ${data.main.temp}, High: ${data.main.temp_max}, Low: ${data.main.temp_min}`);
+  results.append(`${data.name}, ${data.weather[0].main}, Current: ${data.main.temp}, High: ${data.main.temp_max}, Low: ${data.main.temp_min}`);
+
+  city.value = '';
 });
