a12a525f712b532367a55609da907b3faaff1cb3
supersakana <zacwilliamson@icloud.com>
2023-02-01 08:26:10 -0600

    refactored async code to console log loading while fetching data

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,23 @@
+import { toF, toC } from './temp';
+
 async function callApi(city) {
+  console.log('loading');
+  const results = document.querySelector('#results');
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e7775dd9f2432256b7d57eb357896009`, { mode: 'cors' });
     const data = await response.json();
-    return data;
+    results.innerHTML = '';
+
+    results.append(`${data.name},
+                  ${data.weather[0].main},
+                   Current: ${toF(data.main.temp)}/${toC(data.main.temp)},
+                   High: ${toF(data.main.temp_max)}/${toC(data.main.temp_max)},
+                   Low: ${toF(data.main.temp_min)}/${toC(data.main.temp_min)}`);
   } catch {
-    return 'Invalid search';
+    const err = `Invalid search for "${city}"`;
+    results.append(err);
+  } finally {
+    console.log('finished');
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 // import your function
 import callApi from './fetch';
-import { toF, toC } from './temp';
 import './style.css';
 
 document.querySelector('#submit').addEventListener('click', async (e) => {
   e.preventDefault();
-  const results = document.querySelector('#results');
-  results.innerHTML = '';
-
   const city = document.querySelector('#search');
-  const data = await callApi(city.value);
-  results.append(`${data.name},
-                  ${data.weather[0].main},
-                   Current: ${toF(data.main.temp)}/${toC(data.main.temp)},
-                   High: ${toF(data.main.temp_max)}/${toC(data.main.temp_max)},
-                   Low: ${toF(data.main.temp_min)}/${toC(data.main.temp_min)}`);
-
+  callApi(city.value);
   city.value = '';
 });
