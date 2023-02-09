eadd15e39604416a7140785402b035c546f862b2
supersakana <zacwilliamson@icloud.com>
2023-02-09 07:55:03 -0600

    only 5 forcast cards can be produced with each query

--- src/forcast.js
+++ src/forcast.js
@@ -1,14 +1,19 @@
 /* eslint-disable */
+
+// make it so only 5 cards max can be produced
+
 import { toF, toC } from './conversion';
 
 function forcastCards(forcast) {
   let date = '';
   let cards = '';
+  let i = 0
 
   forcast.list.forEach((day) => {
-    if (date != moment(day.dt_txt).format('MMM D')) {
-      cards
-        += `
+    if (date != moment(day.dt_txt).format('MMM D') && i < 5) {
+      i += 1
+      cards += 
+       `
         <div class="bg-white flex flex-col items-center justify-center gap-y-3 py-3 rounded-lg shadow-md">
             <span class="text-md md:text-lg">${moment(day.dt_txt).format('MMM D')}</span> 
             <img class="w-[40px] h-[40px]"src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png">
